5f79d0899a0956a063557f2544aab351a978e87a
ljorge2016 <jluis.emrique@gmail.com>
2021-05-04 10:29:00 -0500

    Creación de la Entidad Usuario

---
+++ BOPE.API/Entidades/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BOPE.API.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+    }
+}
